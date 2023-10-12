7e9121acc177c22f780c115f033e3e8223f68b40
Will McCloy <william.mccloy@googlemail.com>
2023-10-12 13:11:18 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+import Hero from './components/Hero.vue';
+</script>
+<template>
+  <div>
+    <Hero/>
+  </div>
+</template>
+
